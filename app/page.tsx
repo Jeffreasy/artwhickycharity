5db6bc53f8ef2be4b31322669a45e86a5c538a60
Jeffreasy <laventejeffrey@gmail.com>
2025-02-24 15:56:36 +0100

    Remove ClientSearchParams component and update heroicons dependency

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 ﻿import React, { Suspense } from 'react'
 import { Home } from '@/app/home/components/Home'
-import { ClientSearchParams } from '@/components/wrappers/ClientSearchParams'
 import { Loading } from '@/globalComponents/ui/Loading'
 
 export default function Page() {
   return (
     <Suspense fallback={<Loading />}>
-      <ClientSearchParams />
       <Home />
     </Suspense>
   )
