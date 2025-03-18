42761744d8f4d2096b03a2d74c9dec4f7eb3c0a2
Jeffrey <jeffrey@dekoninklijkeloop.nl>
2025-03-18 15:23:06 +0100

    Open shop for testing

--- app/config/shopConfig.ts
+++ app/config/shopConfig.ts
@@ -1,5 +1,5 @@
 export const SHOP_CONFIG = {
-  isEnabled: false, // Zet op true om shop te activeren
+  isEnabled: true, // Zet op true om shop te activeren
   maintenanceMessage: {
     title: "Webshop Coming Soon",
     description: "Our webshop is currently under construction. We're working hard to bring you an amazing selection of whiskies for charity.",
